0104c8a369729182c88d7c72d27773711765b506
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-12-16 12:19:22 +0000

    GetFolder For Repo

---
+++ src/Credfeto.Dotnet.Repo.Git.Tests/GitUtilsTests.cs
@@ -0,0 +1,17 @@
+﻿using FunFair.Test.Common;
+using Xunit;
+
+namespace Credfeto.Dotnet.Repo.Git.Tests;
+
+public sealed class GitUtilsTests : TestBase
+{
+    [Theory]
+    [InlineData("https://github.com/credfeto/credfeto-dotnet-repo-tools", "credfeto-dotnet-repo-tools")]
+    [InlineData("git@github.com:credfeto/credfeto-dotnet-repo-tools.git", "credfeto-dotnet-repo-tools")]
+    public void GetFolderForRepo(string gitUrl, string expected)
+    {
+        string actual = GitUtils.GetFolderForRepo(gitUrl);
+
+        Assert.Equal(expected: expected, actual: actual);
+    }
+}
