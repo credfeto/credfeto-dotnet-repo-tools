02c0f5e04e4af1d49d6c41d99a727c50c929573f
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-12-16 23:16:41 +0000

    Added status tracking project and tests placeholder

---
+++ src/Credfeto.Dotnet.Repo.Tracking.Tests/StatusTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FunFair.Test.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Credfeto.Dotnet.Repo.Tracking.Tests;
+
+public sealed class StatusTests : LoggingFolderCleanupTestBase
+{
+    public StatusTests(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    [Fact]
+    public void Placeholder()
+    {
+        Assert.Empty(Array.Empty<string>());
+    }
+}
