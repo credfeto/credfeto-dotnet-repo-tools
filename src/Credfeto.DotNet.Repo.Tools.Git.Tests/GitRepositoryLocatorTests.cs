f96a57b616783d878ceca95b0fa41006bc430a4e
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-12-28 16:29:10 +0000

    Fixing tests and adding in additional tests

--- src/Credfeto.DotNet.Repo.Tools.Git.Tests/GitRepositoryLocatorTests.cs
+++ src/Credfeto.DotNet.Repo.Tools.Git.Tests/GitRepositoryLocatorTests.cs
@@ -23,12 +23,17 @@ public sealed class GitRepositoryLocatorTests : LoggingTestBase
     [Theory]
     [InlineData(Repositories.GitHubSelfHttps, "credfeto", "credfeto-dotnet-repo-tools")]
     [InlineData(Repositories.GitHubSelfSsh, "credfeto", "credfeto-dotnet-repo-tools")]
-    [InlineData(Repositories.GitHubHttps, "credfeto", "credfeto-dotnet-repo-tools")]
-    [InlineData(Repositories.GitHubSsh, "credfeto", "credfeto-dotnet-repo-tools")]
+    [InlineData(Repositories.GitHubHttps, "credfeto", "scratch")]
+    [InlineData(Repositories.GitHubSsh, "credfeto", "scratch")]
+    [InlineData("https://github.com/meziantou/Meziantou.Analyzer.git", "meziantou", "Meziantou.Analyzer")]
+    [InlineData("git@github.com:meziantou/Meziantou.Analyzer.git", "meziantou", "Meziantou.Analyzer")]
     public void GetFolderForRepo(string gitUrl, string expectedParent, string expectedSub)
     {
-        string actual = this._gitRepositoryLocator.GetWorkingDirectory(workDir: this._basePath, repoUrl: gitUrl);
         string expected = Path.Combine(path1: this._basePath, path2: expectedParent, path3: expectedSub);
+        string actual = this._gitRepositoryLocator.GetWorkingDirectory(workDir: this._basePath, repoUrl: gitUrl);
+
+        this.Output.WriteLine($"Expected: {expected}");
+        this.Output.WriteLine($"Actual  : {actual}");
 
         Assert.Equal(expected: expected, actual: actual);
     }
