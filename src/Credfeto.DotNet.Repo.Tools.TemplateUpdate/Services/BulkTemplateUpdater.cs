286a26c9734df1fe9ea3bdd5d8fd2e915ebb3b5a
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-01-10 18:36:50 +0000

    Added calls to the updaters for code cleanup and template update, even if all they do at the moment is throw not supported

--- src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/Services/BulkTemplateUpdater.cs
+++ src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/Services/BulkTemplateUpdater.cs
@@ -1,7 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Credfeto.DotNet.Repo.Tools.TemplateUpdate.Interfaces;
 
 namespace Credfeto.DotNet.Repo.Tools.TemplateUpdate.Services;
 
 public sealed class BulkTemplateUpdater : IBulkTemplateUpdater
 {
+    public ValueTask BulkUpdateAsync(string templateRepository,
+                                     string trackingFileName,
+                                     string packagesFileName,
+                                     string workFolder,
+                                     string releaseConfigFileName,
+                                     IReadOnlyList<string> repositories,
+                                     CancellationToken cancellationToken)
+    {
+        throw new NotSupportedException("Not yet available");
+    }
 }
