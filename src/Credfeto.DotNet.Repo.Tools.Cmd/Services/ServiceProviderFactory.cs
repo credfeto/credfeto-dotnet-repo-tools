2e39931df994ab47b7b1dc0ce9bb918b75bc7931
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-01-22 14:26:33 +0000

    Fixed analysis issues

--- src/Credfeto.DotNet.Repo.Tools.Cmd/Services/ServiceProviderFactory.cs
+++ src/Credfeto.DotNet.Repo.Tools.Cmd/Services/ServiceProviderFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using Credfeto.DotNet.Repo.Tools.Build.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,6 +7,7 @@ namespace Credfeto.DotNet.Repo.Tools.Cmd.Services;
 
 public sealed class ServiceProviderFactory : IServiceProviderFactory
 {
+    [SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Global", Justification = "False positive - the interface defines the method")]
     public IServiceProvider Build(IServiceCollection serviceCollection)
     {
         return serviceCollection.BuildServiceProvider();
