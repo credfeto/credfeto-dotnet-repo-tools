a854d36e5820ca64ae216eccbf052d2d90cf34ea
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-02-15 10:11:17 +0000

    Rider removed used using stattement on cleanup so code no longer built

--- src/Credfeto.DotNet.Repo.Tools.DotNet/Services/DotNetVersion.cs
+++ src/Credfeto.DotNet.Repo.Tools.DotNet/Services/DotNetVersion.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Credfeto.DotNet.Repo.Tools.DotNet.Interfaces;
+using Credfeto.Extensions.Linq;
 
 namespace Credfeto.DotNet.Repo.Tools.DotNet.Services;
 
