cf8a8540dcb9647ad056b35f8a3fd45aa11630fc
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-02-16 18:12:04 +0000

    More dependabot support

--- src/Credfeto.DotNet.Repo.Tools.Models/RepoContextExtensions.cs
+++ src/Credfeto.DotNet.Repo.Tools.Models/RepoContextExtensions.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
+using Credfeto.Extensions.Linq;
 
 namespace Credfeto.DotNet.Repo.Tools.Models;
 
@@ -63,14 +65,23 @@ public static class RepoContextExtensions
 
     public static bool HasDockerFiles(this in RepoContext repoContext)
     {
-        // TODO: Implement this method
-        return false;
+        return repoContext.HasFiles(searchPattern: "Dockerfile");
     }
 
     public static bool HasPython(this in RepoContext repoContext)
     {
-        // TODO: Implement this method
-        return false;
+        return repoContext.HasFiles(searchPattern: "requirements.txt");
+    }
+
+    private static bool HasFiles(this in RepoContext repoContext, string searchPattern)
+    {
+        return repoContext.GetFiles(searchPattern: searchPattern)
+                          .Any();
+    }
+
+    private static IEnumerable<string> GetFiles(this in RepoContext repoContext, string searchPattern)
+    {
+        return Directory.EnumerateFiles(path: repoContext.WorkingDirectory, searchPattern: searchPattern, searchOption: SearchOption.AllDirectories);
     }
 
     public static bool HasNonStandardGithubActions(this in RepoContext repoContext)
@@ -79,8 +90,31 @@ public static class RepoContextExtensions
         return false;
     }
 
-    public static bool HasNpmAndYarn(this in RepoContext repoContext, [NotNullWhen(true)]out IReadOnlyList<string>? directories)
+    private static IEnumerable<string> GetDirectories(this IEnumerable<string> source, int prefix)
     {
+        return source.Select(Path.GetDirectoryName)
+                     .RemoveNulls()
+                     .Select(file => file.Substring(prefix));
+    }
 
+    public static bool HasNpmAndYarn(this in RepoContext repoContext, [NotNullWhen(true)] out IReadOnlyList<string>? directories)
+    {
+        int prefix = repoContext.WorkingDirectory.Length + 1;
+        IReadOnlyList<string> dirs =
+        [
+            ..repoContext.GetFiles("package.json")
+                         .GetDirectories(prefix)
+        ];
+
+        if (dirs is [])
+        {
+            directories = null;
+
+            return false;
+        }
+
+        directories = dirs;
+
+        return true;
     }
 }
--- src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/Services/DependaBotConfigBuilder.cs
+++ src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/Services/DependaBotConfigBuilder.cs
@@ -20,69 +20,69 @@ public sealed class DependaBotConfigBuilder : IDependaBotConfigBuilder
 
     public ValueTask<string> BuildDependabotConfigAsync(RepoContext repoContext, IReadOnlyList<PackageUpdate> packages, CancellationToken cancellationToken)
     {
-        List<string> config = [
+        List<string> config =
+        [
             "version: 2",
             "updates:"
-            ];
+        ];
 
         if (repoContext.HasSubModules())
         {
-            AddBaseConfig(config: config, ecoSystem: "gitsubmodule", directory: "/", "submodule", "credfeto");
+            this.AddBaseConfig(config: config, ecoSystem: "gitsubmodule", directory: "/", packageTypeLabel: "submodule", reviewer: "credfeto");
             AllowAllDependencies(config);
         }
 
-
-        if (repoContext.HasDotNetFiles(out _, out _, out _))
+        if (repoContext.HasDotNetFiles(sourceDirectory: out _, solutions: out _, projects: out _))
         {
-            AddDotNetConfig(config: config, packages: packages);
+            this.AddDotNetConfig(config: config, packages: packages);
         }
 
-        if(repoContext.HasNpmAndYarn(out IReadOnlyList<string>? directories))
+        if (repoContext.HasNpmAndYarn(out IReadOnlyList<string>? directories))
         {
             foreach (string dir in directories)
             {
-                AddBaseConfig(config: config, ecoSystem: "npm", directory: dir, "npm", "credfeto");
+                this.AddBaseConfig(config: config, ecoSystem: "npm", directory: dir, packageTypeLabel: "npm", reviewer: "credfeto");
                 config.Add("  versioning-strategy: increase-if-necessary");
                 AllowAllDependencies(config);
             }
         }
 
-        if(repoContext.HasDockerFiles())
+        if (repoContext.HasDockerFiles())
         {
-            AddBaseConfig(config: config, ecoSystem: "docker", directory: "/", "docker", "credfeto");
+            this.AddBaseConfig(config: config, ecoSystem: "docker", directory: "/", packageTypeLabel: "docker", reviewer: "credfeto");
             AllowAllDependencies(config);
         }
 
         if (repoContext.HasNonStandardGithubActions())
         {
-            AddBaseConfig(config: config, ecoSystem: "github-actions", directory: "/", "github-actions", "credfeto");
+            this.AddBaseConfig(config: config, ecoSystem: "github-actions", directory: "/", packageTypeLabel: "github-actions", reviewer: "credfeto");
             AllowAllDependencies(config);
         }
 
-        if(repoContext.HasPython())
+        if (repoContext.HasPython())
         {
-            AddBaseConfig(config: config, ecoSystem: "pip", directory: "/", "python", "credfeto");
+            this.AddBaseConfig(config: config, ecoSystem: "pip", directory: "/", packageTypeLabel: "python", reviewer: "credfeto");
             AllowAllDependencies(config);
         }
 
-
-
-        return ValueTask.FromResult(string.Join(Environment.NewLine, config));
+        return ValueTask.FromResult(string.Join(separator: Environment.NewLine, values: config));
     }
 
-    private static void AddDotNetConfig(List<string> config, IReadOnlyList<PackageUpdate> packages)
+    private void AddDotNetConfig(List<string> config, IReadOnlyList<PackageUpdate> packages)
     {
-        AddBaseConfig(config: config, ecoSystem: "github-actions", directory: "/", "nuget", "credfeto");
+        this.AddBaseConfig(config: config, ecoSystem: "github-actions", directory: "/", packageTypeLabel: "nuget", reviewer: "credfeto");
         AllowAllDependencies(config);
 
-        if (packages is not [])
+        if (packages is [])
         {
-            // Add packages to ignore
-            config.Add("  ignore:");
-            config.AddRange(packages.Select(package => package.ExactMatch
-                                                ? $"  - dependency-name: \"{package.PackageId}\""
-                                                : $"  - dependency-name: \"{package.PackageId}.*\""));
+            return;
         }
+
+        // Add packages to ignore
+        config.Add("  ignore:");
+        config.AddRange(packages.Select(package => package.ExactMatch
+                                            ? $"  - dependency-name: \"{package.PackageId}\""
+                                            : $"  - dependency-name: \"{package.PackageId}.*\""));
     }
 
     private static void AllowAllDependencies(List<string> config)
@@ -94,8 +94,10 @@ public sealed class DependaBotConfigBuilder : IDependaBotConfigBuilder
         ]);
     }
 
-    private static void AddBaseConfig(List<string> config, string ecoSystem, string directory, string packageTypeLabel, string reviewer)
+    private void AddBaseConfig(List<string> config, string ecoSystem, string directory, string packageTypeLabel, string reviewer)
     {
+        this._logger.LogInformation($"Adding Dependabot config for {ecoSystem} in {directory}");
+
         config.AddRange([
             "",
             $"- package-ecosystem: {ecoSystem}",
@@ -114,7 +116,7 @@ public sealed class DependaBotConfigBuilder : IDependaBotConfigBuilder
             "  labels:",
             $"  - \"{packageTypeLabel}\"",
             "  - \"dependencies\"",
-            "  - \"Changelog Not Required\"",
+            "  - \"Changelog Not Required\""
         ]);
     }
 }
