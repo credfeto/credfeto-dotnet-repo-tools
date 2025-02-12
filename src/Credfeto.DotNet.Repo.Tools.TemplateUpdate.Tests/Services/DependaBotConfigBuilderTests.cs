8c3f29d2eece9d90f6494c19207c210e61bb2f09
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-02-12 15:41:30 +0000

    Placeholder test

---
+++ src/Credfeto.DotNet.Repo.Tools.TemplateUpdate.Tests/Services/DependaBotConfigBuilderTests.cs
@@ -0,0 +1,23 @@
+using Credfeto.DotNet.Repo.Tools.TemplateUpdate.Services;
+using FunFair.Test.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Credfeto.DotNet.Repo.Tools.TemplateUpdate.Tests.Services;
+
+public abstract class DependaBotConfigBuilderTests : LoggingTestBase
+{
+    private readonly IDependaBotConfigBuilder _dependaBotConfigBuilder;
+
+    public DependaBotConfigBuilderTests(ITestOutputHelper output)
+        : base(output)
+    {
+        this._dependaBotConfigBuilder = new DependaBotConfigBuilder(this.GetTypedLogger<DependaBotConfigBuilder>());
+    }
+
+    [Fact]
+    public void PlaceHolder()
+    {
+        Assert.NotNull(this._dependaBotConfigBuilder);
+    }
+}
