349826c130f22bc2af4ff9fc4b7748f73f3e66e7
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 17:45:13 +0000

    Cleanup: ReleaseConfigExtensions.cs

--- src/Credfeto.DotNet.Repo.Tools.Release/Extensions/ReleaseConfigExtensions.cs
+++ src/Credfeto.DotNet.Repo.Tools.Release/Extensions/ReleaseConfigExtensions.cs
@@ -7,54 +7,32 @@ namespace Credfeto.DotNet.Repo.Tools.Release.Extensions;
 public static class ReleaseConfigExtensions
 {
     // ShouldNeverAutoReleaseRepo Looks at ReleaseConfig.NeverRelease
-    public static bool ShouldNeverAutoReleaseRepo(
-        in this ReleaseConfig releaseConfig,
-        string repoClonePath
-    )
+    public static bool ShouldNeverAutoReleaseRepo(in this ReleaseConfig releaseConfig, string repoClonePath)
     {
-        return releaseConfig.NeverRelease.MatchesPolicy(
-            repoClonePath: repoClonePath,
-            policy: false
-        );
+        return releaseConfig.NeverRelease.MatchesPolicy(repoClonePath: repoClonePath, policy: false);
     }
 
     // CheckRepoForAllowedAutoUpgrade Looks at (NOT ReleaseConfig.NeverRelease AND ReleaseConfig.AlwaysMatch) OR ReleaseConfig.AllowedAutoUpgrade
-    public static bool CheckRepoForAllowedAutoUpgrade(
-        in this ReleaseConfig releaseConfig,
-        string repoClonePath
-    )
+    public static bool CheckRepoForAllowedAutoUpgrade(in this ReleaseConfig releaseConfig, string repoClonePath)
     {
         return releaseConfig.ShouldAlwaysCreatePatchRelease(repoClonePath)
-            || releaseConfig.AllowedAutoUpgrade.MatchesPolicy(
-                repoClonePath: repoClonePath,
-                policy: true
-            );
+            || releaseConfig.AllowedAutoUpgrade.MatchesPolicy(repoClonePath: repoClonePath, policy: true);
     }
 
     // ShouldAlwaysCreatePatchRelease Looks at (NOT ReleaseConfig.NeverRelease AND ReleaseConfig.AlwaysMatch)
-    public static bool ShouldAlwaysCreatePatchRelease(
-        in this ReleaseConfig releaseConfig,
-        string repoClonePath
-    )
+    public static bool ShouldAlwaysCreatePatchRelease(in this ReleaseConfig releaseConfig, string repoClonePath)
     {
         return !releaseConfig.ShouldNeverAutoReleaseRepo(repoClonePath)
             && releaseConfig.ShouldAlwaysReleasePublishable(repoClonePath: repoClonePath);
     }
 
     // ShouldAlwaysReleasePublishable Looks at (ReleaseConfig.AlwaysMatch)
-    public static bool ShouldAlwaysReleasePublishable(
-        in this ReleaseConfig releaseConfig,
-        string repoClonePath
-    )
+    public static bool ShouldAlwaysReleasePublishable(in this ReleaseConfig releaseConfig, string repoClonePath)
     {
         return releaseConfig.AlwaysMatch.MatchesPolicy(repoClonePath: repoClonePath, policy: false);
     }
 
-    private static bool MatchesPolicy(
-        this IReadOnlyList<RepoMatch> grouping,
-        string repoClonePath,
-        bool policy
-    )
+    private static bool MatchesPolicy(this IReadOnlyList<RepoMatch> grouping, string repoClonePath, bool policy)
     {
         return grouping
             .Where(match => match.IsMatch(repoClonePath))
