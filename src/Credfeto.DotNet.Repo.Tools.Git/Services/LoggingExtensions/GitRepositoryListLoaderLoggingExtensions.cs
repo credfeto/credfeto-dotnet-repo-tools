2b3bead418c5b913ab12202df945444581073d50
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 18:50:18 +0000

    Cleanup: GitRepositoryListLoaderLoggingExtensions.cs

--- src/Credfeto.DotNet.Repo.Tools.Git/Services/LoggingExtensions/GitRepositoryListLoaderLoggingExtensions.cs
+++ src/Credfeto.DotNet.Repo.Tools.Git/Services/LoggingExtensions/GitRepositoryListLoaderLoggingExtensions.cs
@@ -4,13 +4,6 @@ namespace Credfeto.DotNet.Repo.Tools.Git.Services.LoggingExtensions;
 
 internal static partial class GitRepositoryListLoaderLoggingExtensions
 {
-    [LoggerMessage(
-        EventId = 1,
-        Level = LogLevel.Information,
-        Message = "Loading repository list from {source}..."
-    )]
-    public static partial void LoadingRepos(
-        this ILogger<GitRepositoryListLoader> logger,
-        string source
-    );
+    [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Loading repository list from {source}...")]
+    public static partial void LoadingRepos(this ILogger<GitRepositoryListLoader> logger, string source);
 }
