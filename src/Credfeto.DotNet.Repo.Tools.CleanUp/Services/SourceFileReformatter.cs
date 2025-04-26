16ee69ebb62eefcaee59702887d04c07a5d1585b
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 16:46:45 +0000

    Cleanup: SourceFileReformatter.cs

--- src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/SourceFileReformatter.cs
+++ src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/SourceFileReformatter.cs
@@ -11,13 +11,13 @@ namespace Credfeto.DotNet.Repo.Tools.CleanUp.Services;
 public sealed class SourceFileReformatter : ISourceFileReformatter
 {
     private static readonly CodeFormatterOptions Options = new()
-                                                           {
-                                                               Width = 120,
-                                                               IndentStyle = IndentStyle.Spaces,
-                                                               EndOfLine = EndOfLine.Auto,
-                                                               IncludeGenerated = false,
-                                                               IndentSize = 4
-                                                           };
+    {
+        Width = 120,
+        IndentStyle = IndentStyle.Spaces,
+        EndOfLine = EndOfLine.Auto,
+        IncludeGenerated = false,
+        IndentSize = 4,
+    };
 
     private readonly ILogger<SourceFileReformatter> _logger;
 
@@ -28,7 +28,11 @@ public sealed class SourceFileReformatter : ISourceFileReformatter
 
     public async ValueTask<string> ReformatAsync(string content, string fileName, CancellationToken cancellationToken)
     {
-        CodeFormatterResult formatted = await CSharpFormatter.FormatAsync(code: content, options: Options, cancellationToken: cancellationToken);
+        CodeFormatterResult formatted = await CSharpFormatter.FormatAsync(
+            code: content,
+            options: Options,
+            cancellationToken: cancellationToken
+        );
 
         if (formatted.CompilationErrors.Any())
         {
