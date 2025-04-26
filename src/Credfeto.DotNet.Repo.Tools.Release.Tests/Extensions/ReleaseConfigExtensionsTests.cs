013ff273961d4603a24ef9554b6b9773b37b8cd0
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 17:36:33 +0000

    Cleanup: ReleaseConfigExtensionsTests.cs

--- src/Credfeto.DotNet.Repo.Tools.Release.Tests/Extensions/ReleaseConfigExtensionsTests.cs
+++ src/Credfeto.DotNet.Repo.Tools.Release.Tests/Extensions/ReleaseConfigExtensionsTests.cs
@@ -15,37 +15,17 @@ public sealed partial class ReleaseConfigExtensionsTests : TestBase
             InactivityHoursBeforeAutoRelease: 9,
             [
                 new(Repo: "template", MatchType: MatchType.CONTAINS, Include: true),
-                new(
-                    Repo: "git@github.com:example/never-release.git",
-                    MatchType: MatchType.EXACT,
-                    Include: true
-                ),
+                new(Repo: "git@github.com:example/never-release.git", MatchType: MatchType.EXACT, Include: true),
             ],
             [
-                new(
-                    Repo: "git@github.com:example/auto-upgrade-true.git",
-                    MatchType: MatchType.EXACT,
-                    Include: true
-                ),
-                new(
-                    Repo: "git@github.com:example/auto-upgrade-false.git",
-                    MatchType: MatchType.EXACT,
-                    Include: false
-                ),
+                new(Repo: "git@github.com:example/auto-upgrade-true.git", MatchType: MatchType.EXACT, Include: true),
+                new(Repo: "git@github.com:example/auto-upgrade-false.git", MatchType: MatchType.EXACT, Include: false),
             ],
             [
                 new(Repo: "credfeto", MatchType: MatchType.CONTAINS, Include: true),
                 new(Repo: "code-analysis", MatchType: MatchType.CONTAINS, Include: false),
-                new(
-                    Repo: "git@github.com:example/never-match.git",
-                    MatchType: MatchType.EXACT,
-                    Include: false
-                ),
-                new(
-                    Repo: "git@github.com:example/always-match.git",
-                    MatchType: MatchType.EXACT,
-                    Include: true
-                ),
+                new(Repo: "git@github.com:example/never-match.git", MatchType: MatchType.EXACT, Include: false),
+                new(Repo: "git@github.com:example/always-match.git", MatchType: MatchType.EXACT, Include: true),
             ]
         );
     }
