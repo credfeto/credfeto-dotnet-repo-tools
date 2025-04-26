40b4df02337b339f431a3bbfdaa0d0dc2756cad1
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 16:02:48 +0000

    Cleanup: VersionCheck.cs

--- src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/VersionCheck.cs
+++ src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/VersionCheck.cs
@@ -4,10 +4,7 @@ namespace Credfeto.DotNet.Repo.Tools.TemplateUpdate;
 
 public static class VersionCheck
 {
-    public static bool IsDotNetSdkTargetNewer(
-        NuGetVersion sourceVersion,
-        NuGetVersion targetVersion
-    )
+    public static bool IsDotNetSdkTargetNewer(NuGetVersion sourceVersion, NuGetVersion targetVersion)
     {
         // Shouldn't need to do more than this, but having it as a method like this allows for future configuration
         return targetVersion > sourceVersion;
