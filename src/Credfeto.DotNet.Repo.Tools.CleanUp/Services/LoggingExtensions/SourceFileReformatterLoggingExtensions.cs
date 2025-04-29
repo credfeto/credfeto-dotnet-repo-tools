768991686c942efb180df1ec5017c144c8ecb85d
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-29 15:21:35 +0000

    Cleanup: SourceFileReformatterLoggingExtensions.cs

--- src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/LoggingExtensions/SourceFileReformatterLoggingExtensions.cs
+++ src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/LoggingExtensions/SourceFileReformatterLoggingExtensions.cs
@@ -9,14 +9,30 @@ namespace Credfeto.DotNet.Repo.Tools.CleanUp.Services.LoggingExtensions;
 internal static partial class SourceFileReformatterLoggingExtensions
 {
     [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "Cleaning: {filename} Errors occured: {errors}")]
-    private static partial void FormattingErrorsFound(this ILogger<SourceFileReformatter> logger, string filename, string errors);
+    private static partial void FormattingErrorsFound(
+        this ILogger<SourceFileReformatter> logger,
+        string filename,
+        string errors
+    );
 
-    public static void FormattingErrorsFound(this ILogger<SourceFileReformatter> logger, string filename, IReadOnlyList<Diagnostic> errors)
+    public static void FormattingErrorsFound(
+        this ILogger<SourceFileReformatter> logger,
+        string filename,
+        IReadOnlyList<Diagnostic> errors
+    )
     {
-        string msg = string.Join(separator: Environment.NewLine, errors.Select(e => $"{e.Id}: {e.Location}: {e.GetMessage()}"));
+        string msg = string.Join(
+            separator: Environment.NewLine,
+            errors.Select(e => $"{e.Id}: {e.Location}: {e.GetMessage()}")
+        );
         logger.FormattingErrorsFound(filename: filename, errors: msg);
     }
 
     [LoggerMessage(EventId = 2, Level = LogLevel.Error, Message = "Cleaning: {filename} Errors occured: {message}")]
-    public static partial void FormattingErrorsFound(this ILogger<SourceFileReformatter> logger, string filename, string message, Exception exception);
+    public static partial void FormattingErrorsFound(
+        this ILogger<SourceFileReformatter> logger,
+        string filename,
+        string message,
+        Exception exception
+    );
 }
