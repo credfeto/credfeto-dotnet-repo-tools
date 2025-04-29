54b34396cb5b5cb79398f8e3763cdec044ca2d8c
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-29 14:02:30 +0100

    Handling exceptions from CSharpier

--- src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/LoggingExtensions/SourceFileReformatterLoggingExtensions.cs
+++ src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/LoggingExtensions/SourceFileReformatterLoggingExtensions.cs
@@ -1,9 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Logging;
 
 namespace Credfeto.DotNet.Repo.Tools.CleanUp.Services.LoggingExtensions;
 
 internal static partial class SourceFileReformatterLoggingExtensions
 {
-    [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "Cleaning: {filename} Errors occured")]
-    public static partial void FormattingErrorsFound(this ILogger<SourceFileReformatter> logger, string filename);
+    [LoggerMessage(EventId = 1, Level = LogLevel.Error, Message = "Cleaning: {filename} Errors occured: {errors}")]
+    private static partial void FormattingErrorsFound(this ILogger<SourceFileReformatter> logger, string filename, string errors);
+
+    public static void FormattingErrorsFound(this ILogger<SourceFileReformatter> logger, string filename, IReadOnlyList<Diagnostic> errors)
+    {
+        string msg = string.Join(separator: Environment.NewLine, errors.Select(e => $"{e.Id}: {e.Location}: {e.GetMessage()}"));
+        logger.FormattingErrorsFound(filename: filename, errors: msg);
+    }
+
+    [LoggerMessage(EventId = 2, Level = LogLevel.Error, Message = "Cleaning: {filename} Errors occured: {message}")]
+    public static partial void FormattingErrorsFound(this ILogger<SourceFileReformatter> logger, string filename, string message, Exception exception);
 }
--- src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/SourceFileReformatter.cs
+++ src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/SourceFileReformatter.cs
@@ -1,9 +1,11 @@
-using System.Linq;
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Credfeto.DotNet.Repo.Tools.CleanUp.Services.LoggingExtensions;
 using CSharpier.Core;
 using CSharpier.Core.CSharp;
+using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Logging;
 
 namespace Credfeto.DotNet.Repo.Tools.CleanUp.Services;
@@ -11,13 +13,13 @@ namespace Credfeto.DotNet.Repo.Tools.CleanUp.Services;
 public sealed class SourceFileReformatter : ISourceFileReformatter
 {
     private static readonly CodeFormatterOptions Options = new()
-    {
-        Width = 120,
-        IndentStyle = IndentStyle.Spaces,
-        EndOfLine = EndOfLine.Auto,
-        IncludeGenerated = false,
-        IndentSize = 4,
-    };
+                                                           {
+                                                               Width = 120,
+                                                               IndentStyle = IndentStyle.Spaces,
+                                                               EndOfLine = EndOfLine.Auto,
+                                                               IncludeGenerated = false,
+                                                               IndentSize = 4
+                                                           };
 
     private readonly ILogger<SourceFileReformatter> _logger;
 
@@ -28,19 +30,26 @@ public sealed class SourceFileReformatter : ISourceFileReformatter
 
     public async ValueTask<string> ReformatAsync(string content, string fileName, CancellationToken cancellationToken)
     {
-        CodeFormatterResult formatted = await CSharpFormatter.FormatAsync(
-            code: content,
-            options: Options,
-            cancellationToken: cancellationToken
-        );
-
-        if (formatted.CompilationErrors.Any())
+        try
         {
-            this._logger.FormattingErrorsFound(fileName);
+            CodeFormatterResult formatted = await CSharpFormatter.FormatAsync(code: content, options: Options, cancellationToken: cancellationToken);
+
+            IReadOnlyList<Diagnostic> errors = [..formatted.CompilationErrors];
+
+            if (errors is [])
+            {
+                return formatted.Code;
+            }
+
+            this._logger.FormattingErrorsFound(filename: fileName, errors: errors);
 
             return content;
         }
+        catch (Exception exception)
+        {
+            this._logger.FormattingErrorsFound(filename: fileName, message: exception.Message, exception: exception);
 
-        return formatted.Code;
+            return content;
+        }
     }
 }
