7362bb6a5fdd33d704ef6301c3790227d9c6b06a
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-06-08 16:13:41 +0100

    feat: Update template configuration loading with HTTP and file loading

--- src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/TemplateUpdateSetup.cs
+++ src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/TemplateUpdateSetup.cs
@@ -1,17 +1,48 @@
-﻿using Credfeto.DotNet.Repo.Tools.TemplateUpdate.Interfaces;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Credfeto.DotNet.Repo.Tools.TemplateUpdate.Interfaces;
 using Credfeto.DotNet.Repo.Tools.TemplateUpdate.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
 
 namespace Credfeto.DotNet.Repo.Tools.TemplateUpdate;
 
 public static class TemplateUpdateSetup
 {
+    private const int RETRY_COUNT = 3;
+    private static readonly TimeSpan ClientTimeout = TimeSpan.FromSeconds(30);
+    private static readonly TimeSpan ClientPolicyTimeout = ClientTimeout.Add(TimeSpan.FromSeconds(1));
+
+    private static readonly TimeSpan SleepDuration = TimeSpan.FromMilliseconds(600);
+
     public static IServiceCollection AddTemplateUpdate(this IServiceCollection services)
     {
         return services.AddSingleton<ITemplateConfigLoader, TemplateConfigLoader>()
                        .AddSingleton<IBulkTemplateUpdater, BulkTemplateUpdater>()
                        .AddSingleton<IFileUpdater, FileUpdater>()
                        .AddSingleton<IDependaBotConfigBuilder, DependaBotConfigBuilder>()
-                       .AddSingleton<ILabelsBuilder, LabelsBuilder>();
+                       .AddSingleton<ILabelsBuilder, LabelsBuilder>()
+                       .AddTemplateConfigLoaderLoaderHttpClient();
+    }
+
+    private static IServiceCollection AddTemplateConfigLoaderLoaderHttpClient(this IServiceCollection services)
+    {
+        return services.AddHttpClient(nameof(TemplateConfigLoader), configureClient: ConfigureClient)
+                       .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+                       .ConfigurePrimaryHttpMessageHandler(configureHandler: CreateHandler)
+                       .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(ClientPolicyTimeout))
+                       .AddTransientHttpErrorPolicy(x => x.WaitAndRetryAsync(retryCount: RETRY_COUNT, sleepDurationProvider: _ => SleepDuration))
+                       .Services;
+    }
+
+    private static HttpClientHandler CreateHandler(IServiceProvider serviceProvider)
+    {
+        return new() { AutomaticDecompression = DecompressionMethods.All };
+    }
+
+    private static void ConfigureClient(HttpClient httpClient)
+    {
+        httpClient.Timeout = ClientTimeout;
     }
 }
