43bae344aee5e941c0ce80024d33a537ccc52d70
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-05 01:54:15 +0000

    Cleanup: TemplateUpdateContext.cs

--- src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/TemplateUpdateContext.cs
+++ src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/TemplateUpdateContext.cs
@@ -12,4 +12,5 @@ public readonly record struct TemplateUpdateContext(
     string TrackingFileName,
     TemplateConfig TemplateConfig,
     DotNetVersionSettings DotNetSettings,
-    ReleaseConfig ReleaseConfig);
+    ReleaseConfig ReleaseConfig
+);
