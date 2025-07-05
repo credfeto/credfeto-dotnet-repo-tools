c991fcc9529830ffeda1ff2e516c111bd43689f5
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-05 01:04:21 +0000

    Cleanup: BuildSettings.cs

--- src/Credfeto.DotNet.Repo.Tools.Build.Interfaces/BuildSettings.cs
+++ src/Credfeto.DotNet.Repo.Tools.Build.Interfaces/BuildSettings.cs
@@ -4,7 +4,11 @@ using System.Diagnostics;
 namespace Credfeto.DotNet.Repo.Tools.Build.Interfaces;
 
 [DebuggerDisplay("Publishable: {Publishable}, Packable: {Packable}, Framework: {Framework}")]
-public readonly record struct BuildSettings(IReadOnlyList<string> PublishableProjects, IReadOnlyList<string> PackableProjects, string? Framework)
+public readonly record struct BuildSettings(
+    IReadOnlyList<string> PublishableProjects,
+    IReadOnlyList<string> PackableProjects,
+    string? Framework
+)
 {
     public bool Publishable => this.PublishableProjects.Count != 0;
 
