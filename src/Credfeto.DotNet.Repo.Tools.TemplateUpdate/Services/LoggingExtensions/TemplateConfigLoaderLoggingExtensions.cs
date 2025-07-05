9824d5c9c4a57af9f7147dc47fd40a007f8e44b3
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-05 04:02:01 +0000

    Cleanup: TemplateConfigLoaderLoggingExtensions.cs

--- src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/Services/LoggingExtensions/TemplateConfigLoaderLoggingExtensions.cs
+++ src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/Services/LoggingExtensions/TemplateConfigLoaderLoggingExtensions.cs
@@ -4,6 +4,10 @@ namespace Credfeto.DotNet.Repo.Tools.TemplateUpdate.Services.LoggingExtensions;
 
 internal static partial class TemplateConfigLoaderLoggingExtensions
 {
-    [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Loading template configuration from {source}...")]
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Information,
+        Message = "Loading template configuration from {source}..."
+    )]
     public static partial void LoadingTemplateConfig(this ILogger<TemplateConfigLoader> logger, string source);
 }
