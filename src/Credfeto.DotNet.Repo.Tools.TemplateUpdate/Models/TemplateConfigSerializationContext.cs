939cb40a66278772304be8367404139a30d65830
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-05 03:17:40 +0000

    Cleanup: TemplateConfigSerializationContext.cs

--- src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/Models/TemplateConfigSerializationContext.cs
+++ src/Credfeto.DotNet.Repo.Tools.TemplateUpdate/Models/TemplateConfigSerializationContext.cs
@@ -3,12 +3,18 @@ using System.Text.Json.Serialization;
 
 namespace Credfeto.DotNet.Repo.Tools.TemplateUpdate.Models;
 
-[SuppressMessage(category: "ReSharper", checkId: "PartialTypeWithSinglePart", Justification = "Required for JsonSerializerContext")]
-[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Serialization | JsonSourceGenerationMode.Metadata,
-                             PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-                             DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-                             WriteIndented = false,
-                             IncludeFields = false)]
+[SuppressMessage(
+    category: "ReSharper",
+    checkId: "PartialTypeWithSinglePart",
+    Justification = "Required for JsonSerializerContext"
+)]
+[JsonSourceGenerationOptions(
+    GenerationMode = JsonSourceGenerationMode.Serialization | JsonSourceGenerationMode.Metadata,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    WriteIndented = false,
+    IncludeFields = false
+)]
 [JsonSerializable(typeof(CleanupTemplateConfig))]
 [JsonSerializable(typeof(DependabotTemplateConfig))]
 [JsonSerializable(typeof(GitHubTemplateConfig))]
