787a4e5f79a2462db1e9f733569b74c1cf2b7c42
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-05 01:27:20 +0000

    Cleanup: DependencyInjectionTests.cs

--- src/Credfeto.DotNet.Repo.Tools.Git.Tests/DependencyInjectionTests.cs
+++ src/Credfeto.DotNet.Repo.Tools.Git.Tests/DependencyInjectionTests.cs
@@ -1,4 +1,4 @@
-﻿using Credfeto.DotNet.Repo.Tools.Git.Interfaces;
+using Credfeto.DotNet.Repo.Tools.Git.Interfaces;
 using FunFair.Test.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -8,9 +8,7 @@ namespace Credfeto.DotNet.Repo.Tools.Git.Tests;
 public sealed class DependencyInjectionTests : DependencyInjectionTestsBase
 {
     public DependencyInjectionTests(ITestOutputHelper output)
-        : base(output: output, dependencyInjectionRegistration: Configure)
-    {
-    }
+        : base(output: output, dependencyInjectionRegistration: Configure) { }
 
     [Fact]
     public void GitRepositoryLocatorMustBeRegistered()
