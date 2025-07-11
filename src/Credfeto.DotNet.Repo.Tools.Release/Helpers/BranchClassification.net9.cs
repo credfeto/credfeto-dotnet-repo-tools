cd098f5ffe357f561e6e65ab8bd7f411c6d6ce88
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-11 14:30:08 +0000

    Cleanup: BranchClassification.net9.cs

--- src/Credfeto.DotNet.Repo.Tools.Release/Helpers/BranchClassification.net9.cs
+++ src/Credfeto.DotNet.Repo.Tools.Release/Helpers/BranchClassification.net9.cs
@@ -6,13 +6,14 @@ namespace Credfeto.DotNet.Repo.Tools.Release.Helpers;
 
 internal static partial class BranchClassification
 {
-    private static readonly SearchValues<string>
-        DependencyBranches = SearchValues.Create([PACKAGE_UPDATER_BRANCH_PREFIX, DEPENDABOT_BRANCH_PREFIX], comparisonType: StringComparison.OrdinalIgnoreCase);
+    private static readonly SearchValues<string> DependencyBranches = SearchValues.Create(
+        [PACKAGE_UPDATER_BRANCH_PREFIX, DEPENDABOT_BRANCH_PREFIX],
+        comparisonType: StringComparison.OrdinalIgnoreCase
+    );
 
     public static bool IsDependencyBranch(string branch)
     {
-        return branch.AsSpan()
-                     .IndexOfAny(DependencyBranches) == 0 && !IsDotNetSdkPreviewUpdate(branch);
+        return branch.AsSpan().IndexOfAny(DependencyBranches) == 0 && !IsDotNetSdkPreviewUpdate(branch);
     }
 }
 #endif
