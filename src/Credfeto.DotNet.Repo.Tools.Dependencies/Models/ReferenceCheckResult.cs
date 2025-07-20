56a3231225f8da1ed8f725c7d8523778979410ef
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-20 20:02:38 +0100

    Backup

--- src/Credfeto.DotNet.Repo.Tools.Dependencies/Models/ReferenceCheckResult.cs
+++ src/Credfeto.DotNet.Repo.Tools.Dependencies/Models/ReferenceCheckResult.cs
@@ -4,4 +4,12 @@ using System.IO;
 namespace Credfeto.DotNet.Repo.Tools.Dependencies.Models;
 
 [DebuggerDisplay("File: {File.FullName}, Type: {Type} Name {Name} Version: {Version}")]
-internal sealed record ReferenceCheckResult(FileInfo File, ReferenceType Type, string Name, string? Version);
+internal sealed record ReferenceCheckResult(FileInfo File, ReferenceType Type, string Name, string? Version)
+{
+    public ReferenceCheckResult(FileInfo File, ReferenceType Type, string Name)
+    : this(File, Type, Name, null)
+    {
+
+    }
+
+};
