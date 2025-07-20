7bec22d10c3a9c627f3a68417fc1d992e18431c1
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-20 16:32:00 +0100

    Added interface and tests

---
+++ src/Credfeto.DotNet.Repo.Tools.Dependencies.Interfaces/IBulkCodeCleanUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Credfeto.DotNet.Repo.Tools.Dependencies.Interfaces;
+
+public interface IBulkDependencyReducer
+{
+    ValueTask BulkUpdateAsync(string templateRepository,
+                              string trackingFileName,
+                              string packagesFileName,
+                              string workFolder,
+                              string releaseConfigFileName,
+                              IReadOnlyList<string> repositories,
+                              CancellationToken cancellationToken);
+}
---
+++ src/Credfeto.DotNet.Repo.Tools.Dependencies.Tests/PlaceholderTests.cs
@@ -0,0 +1,13 @@
+﻿using FunFair.Test.Common;
+using Xunit;
+
+namespace Credfeto.DotNet.Repo.Tools.Dependencies.Tests;
+
+public sealed class PlaceholderTests : TestBase
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Skip("Placeholder");
+    }
+}
