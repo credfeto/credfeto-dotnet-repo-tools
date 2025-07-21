278eb4ba5e229946671257f2c8e76d4bd613fdf0
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-21 22:05:49 +0000

    Cleanup: IBulkDependencyReducer.cs

--- src/Credfeto.DotNet.Repo.Tools.Dependencies.Interfaces/IBulkDependencyReducer.cs
+++ src/Credfeto.DotNet.Repo.Tools.Dependencies.Interfaces/IBulkDependencyReducer.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,11 +6,13 @@ namespace Credfeto.DotNet.Repo.Tools.Dependencies.Interfaces;
 
 public interface IBulkDependencyReducer
 {
-    ValueTask BulkUpdateAsync(string templateRepository,
-                              string trackingFileName,
-                              string packagesFileName,
-                              string workFolder,
-                              string releaseConfigFileName,
-                              IReadOnlyList<string> repositories,
-                              CancellationToken cancellationToken);
+    ValueTask BulkUpdateAsync(
+        string templateRepository,
+        string trackingFileName,
+        string packagesFileName,
+        string workFolder,
+        string releaseConfigFileName,
+        IReadOnlyList<string> repositories,
+        CancellationToken cancellationToken
+    );
 }
