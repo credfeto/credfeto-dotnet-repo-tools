c4c8652e680ff5a125f42fff500e2618892f2534
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-23 23:42:30 +0100

    IProjectFinder.cs

--- src/Credfeto.DotNet.Repo.Tools.Build.Interfaces/IProjectFinder.cs
+++ src/Credfeto.DotNet.Repo.Tools.Build.Interfaces/IProjectFinder.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Credfeto.DotNet.Repo.Tools.Build.Interfaces;
 
 public interface IProjectFinder
 {
-    ValueTask<IReadOnlyList<string>> FindProjectsAsync(string basePath);
+    ValueTask<IReadOnlyList<string>> FindProjectsAsync(string basePath, CancellationToken cancellationToken);
 }
