c5111392d8c0561c262a2edef3f4f2d7f771e7dc
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-24 23:38:03 +0100

    Simplifiying methods

--- src/Credfeto.DotNet.Repo.Tools.Dependencies/Services/LoggingExtensions/DependencyReducerLoggingExtensions.cs
+++ src/Credfeto.DotNet.Repo.Tools.Dependencies/Services/LoggingExtensions/DependencyReducerLoggingExtensions.cs
@@ -6,4 +6,13 @@ internal static partial class DependencyReducerLoggingExtensions
 {
     [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Number of projects to check: {projectCount}")]
     public static partial void ProjectsToCheck(this ILogger<DependencyReducer> logger, int projectCount);
+
+    [LoggerMessage(EventId = 2, Level = LogLevel.Information, Message = "Checking Projects")]
+    public static partial void CheckingProjects(this ILogger<DependencyReducer> logger);
+
+    [LoggerMessage(EventId = 3, Level = LogLevel.Information, Message = "({projectInstance}/{projectCount}): Testing project: {project}")]
+    public static partial void StartTestingProject(this ILogger<DependencyReducer> logger, int projectInstance, int projectCount, string project);
+
+    [LoggerMessage(EventId = 4, Level = LogLevel.Error, Message = "* Does not build without changes")]
+    public static partial void DoesNotBuildWithoutChanges(this ILogger<DependencyReducer> logger);
 }
