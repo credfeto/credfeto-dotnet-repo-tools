b384c3d66542fe57a297f0942fa8be583d048a25
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-05 13:10:20 +0000

    Cleanup: DependencyReductionUpdateContext.cs

--- src/Credfeto.DotNet.Repo.Tools.Dependencies/Models/DependencyReductionUpdateContext.cs
+++ src/Credfeto.DotNet.Repo.Tools.Dependencies/Models/DependencyReductionUpdateContext.cs
@@ -4,4 +4,8 @@ using Credfeto.DotNet.Repo.Tools.DotNet.Interfaces;
 namespace Credfeto.DotNet.Repo.Tools.Dependencies.Models;
 
 [DebuggerDisplay("Work: {WorkFolder}")]
-public readonly record struct DependencyReductionUpdateContext(string WorkFolder, string TrackingFileName, DotNetVersionSettings DotNetSettings);
+public readonly record struct DependencyReductionUpdateContext(
+    string WorkFolder,
+    string TrackingFileName,
+    DotNetVersionSettings DotNetSettings
+);
