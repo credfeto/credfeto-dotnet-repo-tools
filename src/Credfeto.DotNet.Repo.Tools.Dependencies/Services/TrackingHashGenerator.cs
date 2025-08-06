1d106327a15f738a835c1ab8e36f7f2eec1fb137
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-06 12:40:28 +0000

    Cleanup: TrackingHashGenerator.cs

--- src/Credfeto.DotNet.Repo.Tools.Dependencies/Services/TrackingHashGenerator.cs
+++ src/Credfeto.DotNet.Repo.Tools.Dependencies/Services/TrackingHashGenerator.cs
@@ -11,37 +11,64 @@ namespace Credfeto.DotNet.Repo.Tools.Dependencies.Services;
 
 public sealed class TrackingHashGenerator : ITrackingHashGenerator
 {
-    private static readonly IReadOnlyList<string> FileMasks = ["*.sln", "*.slnx", "*.csproj", "*.csproj", "global.json", "*.props", "*.ruleset"];
+    private static readonly IReadOnlyList<string> FileMasks =
+    [
+        "*.sln",
+        "*.slnx",
+        "*.csproj",
+        "*.csproj",
+        "global.json",
+        "*.props",
+        "*.ruleset",
+    ];
 
-    public async ValueTask<string> GenerateTrackingHashAsync(RepoContext repoContext, CancellationToken cancellationToken)
+    public async ValueTask<string> GenerateTrackingHashAsync(
+        RepoContext repoContext,
+        CancellationToken cancellationToken
+    )
     {
         IReadOnlyList<string> sourceFiles = GetFileList(repoContext.WorkingDirectory);
 
         using (HashAlgorithm hashAlgorithm = SHA256.Create())
         {
-            await HashFileListAsync(hashAlgorithm: hashAlgorithm, sourceFiles: sourceFiles, cancellationToken: cancellationToken);
+            await HashFileListAsync(
+                hashAlgorithm: hashAlgorithm,
+                sourceFiles: sourceFiles,
+                cancellationToken: cancellationToken
+            );
 
             return hashAlgorithm.Hash is null
                 ? repoContext.Repository.HeadRev
                 : Convert.ToBase64String(hashAlgorithm.Hash);
         }
-
     }
 
-    private static async ValueTask HashFileListAsync(HashAlgorithm hashAlgorithm, IReadOnlyList<string> sourceFiles, CancellationToken cancellationToken)
+    private static async ValueTask HashFileListAsync(
+        HashAlgorithm hashAlgorithm,
+        IReadOnlyList<string> sourceFiles,
+        CancellationToken cancellationToken
+    )
     {
         await using (CryptoStream cryptoStream = new(Stream.Null, hashAlgorithm, CryptoStreamMode.Write))
         {
             foreach (string fileName in sourceFiles)
             {
-                await HashOneFileAsync(cryptoStream: cryptoStream, fileName: fileName, cancellationToken: cancellationToken);
+                await HashOneFileAsync(
+                    cryptoStream: cryptoStream,
+                    fileName: fileName,
+                    cancellationToken: cancellationToken
+                );
             }
 
             await cryptoStream.FlushFinalBlockAsync(cancellationToken);
         }
     }
 
-    private static async ValueTask HashOneFileAsync(CryptoStream cryptoStream, string fileName, CancellationToken cancellationToken)
+    private static async ValueTask HashOneFileAsync(
+        CryptoStream cryptoStream,
+        string fileName,
+        CancellationToken cancellationToken
+    )
     {
         await using (FileStream fileStream = File.OpenRead(fileName))
         {
@@ -53,8 +80,15 @@ public sealed class TrackingHashGenerator : ITrackingHashGenerator
     {
         return
         [
-            .. FileMasks.SelectMany(filter => Directory.GetFiles(path: sourceFolder, searchPattern: filter, searchOption: SearchOption.AllDirectories))
-                        .Order(StringComparer.OrdinalIgnoreCase)
+            .. FileMasks
+                .SelectMany(filter =>
+                    Directory.GetFiles(
+                        path: sourceFolder,
+                        searchPattern: filter,
+                        searchOption: SearchOption.AllDirectories
+                    )
+                )
+                .Order(StringComparer.OrdinalIgnoreCase),
         ];
     }
 }
