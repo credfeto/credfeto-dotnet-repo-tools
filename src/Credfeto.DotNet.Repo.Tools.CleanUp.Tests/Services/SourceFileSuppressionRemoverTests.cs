85cc0115fb587ffb062e4184883703fed172303c
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-28 18:41:47 +0000

    Additional test

--- src/Credfeto.DotNet.Repo.Tools.CleanUp.Tests/Services/SourceFileSuppressionRemoverTests.cs
+++ src/Credfeto.DotNet.Repo.Tools.CleanUp.Tests/Services/SourceFileSuppressionRemoverTests.cs
@@ -34,4 +34,39 @@ public static class Test {
 
         Assert.Equal(expected: source, actual: actual);
     }
+
+    [Fact]
+    public async Task OneSuppressionShouldBeRemovedAsync()
+    {
+        const string source = @"
+using System.Diagnostics;
+
+namespace Test;
+
+public static class Test {
+
+    [SuppressMessage(category: ""Meziantou.Analyzer"", checkId: ""MA0051: Method is too long"", Justification = ""Unit tests"")]
+    public static void DoesNothing() {
+          // Example
+    }
+}
+";
+
+        const string expected = @"
+using System.Diagnostics;
+
+namespace Test;
+
+public static class Test {
+
+    public static void DoesNothing() {
+          // Example
+    }
+}
+";
+
+        string actual = await this._sourceFileSuppressionRemover.RemoveSuppressionsAsync(fileName: "example.txt", content: source, this.CancellationToken());
+
+        Assert.Equal(expected: expected, actual: actual);
+    }
 }
