e00fb9d478b3ab956424796212136ddf586521ec
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-03-01 01:25:18 +0000

    Cleanup: DotNetFiles.cs

--- src/Credfeto.DotNet.Repo.Tools.Build.Interfaces/DotNetFiles.cs
+++ src/Credfeto.DotNet.Repo.Tools.Build.Interfaces/DotNetFiles.cs
@@ -4,7 +4,11 @@ using System.Diagnostics;
 namespace Credfeto.DotNet.Repo.Tools.Build.Interfaces;
 
 [DebuggerDisplay("Folder: {SourceDirectory} Solutions: {HasSolutions} Projects: {HasProjects}")]
-public readonly record struct DotNetFiles(string SourceDirectory, IReadOnlyList<string> Solutions, IReadOnlyList<string> Projects)
+public readonly record struct DotNetFiles(
+    string SourceDirectory,
+    IReadOnlyList<string> Solutions,
+    IReadOnlyList<string> Projects
+)
 {
     public bool HasSolutions => this.Solutions is not [];
 
