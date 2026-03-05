d867b6fbabfb87da0a1b5073299b7e1c1360e396
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-03-05 16:17:09 +0000

    Cleanup: BuildContext.cs

--- src/Credfeto.DotNet.Repo.Tools.Build.Interfaces/BuildContext.cs
+++ src/Credfeto.DotNet.Repo.Tools.Build.Interfaces/BuildContext.cs
@@ -3,4 +3,8 @@ using System.Diagnostics;
 namespace Credfeto.DotNet.Repo.Tools.Build.Interfaces;
 
 [DebuggerDisplay("Source: {SourceDirectory}")]
-public readonly record struct BuildContext(string SourceDirectory, BuildSettings BuildSettings, BuildOverride BuildOverride);
+public readonly record struct BuildContext(
+    string SourceDirectory,
+    BuildSettings BuildSettings,
+    BuildOverride BuildOverride
+);
