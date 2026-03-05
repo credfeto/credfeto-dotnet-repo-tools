74ceaa02e9d52925449ca2b86e438aaf51783c0e
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-03-05 17:19:17 +0000

    Cleanup: Commands.cs

--- src/Credfeto.DotNet.Repo.Tools.Cmd/Commands.cs
+++ src/Credfeto.DotNet.Repo.Tools.Cmd/Commands.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
@@ -17,8 +17,11 @@ using Microsoft.Extensions.Logging;
 
 namespace Credfeto.DotNet.Repo.Tools.Cmd;
 
-[SuppressMessage(category: "Microsoft.Performance", checkId: "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instantiated by Cocona")]
-
+[SuppressMessage(
+    category: "Microsoft.Performance",
+    checkId: "CA1812:AvoidUninstantiatedInternalClasses",
+    Justification = "Instantiated by Cocona"
+)]
 internal sealed class Commands
 {
     private readonly IBulkCodeCleanUp _bulkCodeCleanUp;
@@ -31,15 +34,21 @@ internal sealed class Commands
     private readonly IGitRepositoryListLoader _gitRepositoryListLoader;
     private readonly ILogger<Commands> _logger;
 
-    [SuppressMessage(category: "FunFair.CodeAnalysis", checkId: "FFS0023: Use ILogger rather than ILogger<T>", Justification = "Needed in this case")]
-    public Commands(IGitRepositoryListLoader gitRepositoryListLoader,
-                    IBulkCodeCleanUp bulkCodeCleanUp,
-                    IBulkPackageUpdater bulkPackageUpdater,
-                    IBulkTemplateUpdater bulkTemplateUpdater,
-                    IBulkDependencyReducer bulkDependencyReducer,
-                    IDependencyReducer dependencyReducer,
-                    IDotNetFilesDetector dotNetFilesDetector,
-                    ILogger<Commands> logger)
+    [SuppressMessage(
+        category: "FunFair.CodeAnalysis",
+        checkId: "FFS0023: Use ILogger rather than ILogger<T>",
+        Justification = "Needed in this case"
+    )]
+    public Commands(
+        IGitRepositoryListLoader gitRepositoryListLoader,
+        IBulkCodeCleanUp bulkCodeCleanUp,
+        IBulkPackageUpdater bulkPackageUpdater,
+        IBulkTemplateUpdater bulkTemplateUpdater,
+        IBulkDependencyReducer bulkDependencyReducer,
+        IDependencyReducer dependencyReducer,
+        IDotNetFilesDetector dotNetFilesDetector,
+        ILogger<Commands> logger
+    )
     {
         this._gitRepositoryListLoader = gitRepositoryListLoader;
         this._bulkCodeCleanUp = bulkCodeCleanUp;
@@ -52,33 +61,40 @@ internal sealed class Commands
     }
 
     [Command("update-packages", Description = "Update all packages in all repositories")]
-    
-    public async Task UpdatePackagesAsync([Option(name: "repositories", ['r'], Description = "repos.lst file containing list of repositories")] string repositoriesFileName,
-                                          [Option(name: "template", ['m'], Description = "Template repository to clone")] string templateRepository,
-                                          [Option(name: "cache", ['c'], Description = "package cache file")] string? cacheFileName,
-                                          [Option(name: "tracking", ['t'], Description = "folder where to write tracking.json file")] string trackingFileName,
-                                          [Option(name: "packages", ['p'], Description = "Packages.json file to load")] string packagesFileName,
-                                          [Option(name: "work", ['w'], Description = "folder where to clone repositories")] string workFolder,
-                                          [Option(name: "release", ['l'], Description = "release.config file to load")] string releaseConfigFileName,
-                                          [Option(name: "source", ['s'], Description = "Urls to additional NuGet feeds to load")] IEnumerable<string>? source)
+    public async Task UpdatePackagesAsync(
+        [Option(name: "repositories", ['r'], Description = "repos.lst file containing list of repositories")]
+            string repositoriesFileName,
+        [Option(name: "template", ['m'], Description = "Template repository to clone")] string templateRepository,
+        [Option(name: "cache", ['c'], Description = "package cache file")] string? cacheFileName,
+        [Option(name: "tracking", ['t'], Description = "folder where to write tracking.json file")]
+            string trackingFileName,
+        [Option(name: "packages", ['p'], Description = "Packages.json file to load")] string packagesFileName,
+        [Option(name: "work", ['w'], Description = "folder where to clone repositories")] string workFolder,
+        [Option(name: "release", ['l'], Description = "release.config file to load")] string releaseConfigFileName,
+        [Option(name: "source", ['s'], Description = "Urls to additional NuGet feeds to load")]
+            IEnumerable<string>? source
+    )
     {
         IReadOnlyList<string> repositories = await this.LoadRepositoriesAsync(
             repositoriesFileName: repositoriesFileName,
             templateRepository: templateRepository,
-            cancellationToken: this._cancellationToken);
+            cancellationToken: this._cancellationToken
+        );
 
         this.Dump(repositories);
 
         IReadOnlyList<string> nugetSources = source?.ToArray() ?? [];
-        await this._bulkPackageUpdater.BulkUpdateAsync(templateRepository: templateRepository,
-                                                       cacheFileName: cacheFileName,
-                                                       trackingFileName: trackingFileName,
-                                                       packagesFileName: packagesFileName,
-                                                       workFolder: workFolder,
-                                                       releaseConfigFileName: releaseConfigFileName,
-                                                       additionalNugetSources: nugetSources,
-                                                       repositories: repositories,
-                                                       cancellationToken: this._cancellationToken);
+        await this._bulkPackageUpdater.BulkUpdateAsync(
+            templateRepository: templateRepository,
+            cacheFileName: cacheFileName,
+            trackingFileName: trackingFileName,
+            packagesFileName: packagesFileName,
+            workFolder: workFolder,
+            releaseConfigFileName: releaseConfigFileName,
+            additionalNugetSources: nugetSources,
+            repositories: repositories,
+            cancellationToken: this._cancellationToken
+        );
 
         this.Done();
     }
@@ -89,43 +105,58 @@ internal sealed class Commands
     }
 
     [Command("update-template", Description = "Update repos from template in all repositories")]
-    
-    public async Task UpdateFromTemplateAsync([Option(name: "repositories", ['r'], Description = "repos.lst file containing list of repositories")] string repositoriesFileName,
-                                              [Option(name: "template", ['m'], Description = "Template repository to clone")] string templateRepository,
-                                              [Option(name: "template-config", ['c'], Description = "template.config file to load")] string templateConfigFileName,
-                                              [Option(name: "tracking", ['t'], Description = "folder where to write tracking.json file")] string trackingFileName,
-                                              [Option(name: "packages", ['p'], Description = "Packages.json file to load")] string packagesFileName,
-                                              [Option(name: "work", ['w'], Description = "folder where to clone repositories")] string workFolder,
-                                              [Option(name: "release", ['l'], Description = "release.config file to load")] string releaseConfigFileName)
+    public async Task UpdateFromTemplateAsync(
+        [Option(name: "repositories", ['r'], Description = "repos.lst file containing list of repositories")]
+            string repositoriesFileName,
+        [Option(name: "template", ['m'], Description = "Template repository to clone")] string templateRepository,
+        [Option(name: "template-config", ['c'], Description = "template.config file to load")]
+            string templateConfigFileName,
+        [Option(name: "tracking", ['t'], Description = "folder where to write tracking.json file")]
+            string trackingFileName,
+        [Option(name: "packages", ['p'], Description = "Packages.json file to load")] string packagesFileName,
+        [Option(name: "work", ['w'], Description = "folder where to clone repositories")] string workFolder,
+        [Option(name: "release", ['l'], Description = "release.config file to load")] string releaseConfigFileName
+    )
     {
         IReadOnlyList<string> repositories = await this.LoadRepositoriesAsync(
             repositoriesFileName: repositoriesFileName,
             templateRepository: templateRepository,
-            cancellationToken: this._cancellationToken);
+            cancellationToken: this._cancellationToken
+        );
 
         this.Dump(repositories);
 
-        await this._bulkTemplateUpdater.BulkUpdateAsync(templateRepository: templateRepository,
-                                                        trackingFileName: trackingFileName,
-                                                        packagesFileName: packagesFileName,
-                                                        workFolder: workFolder,
-                                                        templateConfigFileName: templateConfigFileName,
-                                                        releaseConfigFileName: releaseConfigFileName,
-                                                        repositories: repositories,
-                                                        cancellationToken: this._cancellationToken);
+        await this._bulkTemplateUpdater.BulkUpdateAsync(
+            templateRepository: templateRepository,
+            trackingFileName: trackingFileName,
+            packagesFileName: packagesFileName,
+            workFolder: workFolder,
+            templateConfigFileName: templateConfigFileName,
+            releaseConfigFileName: releaseConfigFileName,
+            repositories: repositories,
+            cancellationToken: this._cancellationToken
+        );
 
         this.Done();
     }
 
-    private async ValueTask<IReadOnlyList<string>> LoadRepositoriesAsync(string repositoriesFileName, string templateRepository, CancellationToken cancellationToken)
+    private async ValueTask<IReadOnlyList<string>> LoadRepositoriesAsync(
+        string repositoriesFileName,
+        string templateRepository,
+        CancellationToken cancellationToken
+    )
     {
-        IReadOnlyList<string> source = await this._gitRepositoryListLoader.LoadAsync(path: repositoriesFileName, cancellationToken: cancellationToken);
+        IReadOnlyList<string> source = await this._gitRepositoryListLoader.LoadAsync(
+            path: repositoriesFileName,
+            cancellationToken: cancellationToken
+        );
 
-        IReadOnlyList<string> repositories = ExcludeTemplateRepo(repositories: source, templateRepository: templateRepository);
+        IReadOnlyList<string> repositories = ExcludeTemplateRepo(
+            repositories: source,
+            templateRepository: templateRepository
+        );
 
-        return repositories is []
-            ? NoRepositories()
-            : repositories;
+        return repositories is [] ? NoRepositories() : repositories;
     }
 
     [DoesNotReturn]
@@ -135,62 +166,82 @@ internal sealed class Commands
     }
 
     [Command("code-cleanup", Description = "Perform code cleanup in all repositories")]
-    
-    public async Task CodeCleanupAsync([Option(name: "repositories", ['r'], Description = "repos.lst file containing list of repositories")] string repositoriesFileName,
-                                       [Option(name: "template", ['m'], Description = "Template repository to clone")] string templateRepository,
-                                       [Option(name: "tracking", ['t'], Description = "folder where to write tracking.json file")] string trackingFileName,
-                                       [Option(name: "packages", ['p'], Description = "Packages.json file to load")] string packagesFileName,
-                                       [Option(name: "work", ['w'], Description = "folder where to clone repositories")] string workFolder,
-                                       [Option(name: "release", ['l'], Description = "release.config file to load")] string releaseConfigFileName)
+    public async Task CodeCleanupAsync(
+        [Option(name: "repositories", ['r'], Description = "repos.lst file containing list of repositories")]
+            string repositoriesFileName,
+        [Option(name: "template", ['m'], Description = "Template repository to clone")] string templateRepository,
+        [Option(name: "tracking", ['t'], Description = "folder where to write tracking.json file")]
+            string trackingFileName,
+        [Option(name: "packages", ['p'], Description = "Packages.json file to load")] string packagesFileName,
+        [Option(name: "work", ['w'], Description = "folder where to clone repositories")] string workFolder,
+        [Option(name: "release", ['l'], Description = "release.config file to load")] string releaseConfigFileName
+    )
     {
         IReadOnlyList<string> repositories = await this.LoadRepositoriesAsync(
             repositoriesFileName: repositoriesFileName,
             templateRepository: templateRepository,
-            cancellationToken: this._cancellationToken);
+            cancellationToken: this._cancellationToken
+        );
 
         this.Dump(repositories);
 
-        await this._bulkCodeCleanUp.BulkUpdateAsync(templateRepository: templateRepository,
-                                                    trackingFileName: trackingFileName,
-                                                    packagesFileName: packagesFileName,
-                                                    workFolder: workFolder,
-                                                    releaseConfigFileName: releaseConfigFileName,
-                                                    repositories: repositories,
-                                                    cancellationToken: this._cancellationToken);
+        await this._bulkCodeCleanUp.BulkUpdateAsync(
+            templateRepository: templateRepository,
+            trackingFileName: trackingFileName,
+            packagesFileName: packagesFileName,
+            workFolder: workFolder,
+            releaseConfigFileName: releaseConfigFileName,
+            repositories: repositories,
+            cancellationToken: this._cancellationToken
+        );
 
         this.Done();
     }
 
     [Command("reduce-dependencies", Description = "Reduce dependencies in all repositories")]
-    
-    public async Task ReduceDependenciesAsync([Option(name: "repositories", ['r'], Description = "repos.lst file containing list of repositories")] string repositoriesFileName,
-                                              [Option(name: "template", ['m'], Description = "Template repository to clone")] string templateRepository,
-                                              [Option(name: "tracking", ['t'], Description = "folder where to write tracking.json file")] string trackingFileName,
-                                              [Option(name: "work", ['w'], Description = "folder where to clone repositories")] string workFolder)
+    public async Task ReduceDependenciesAsync(
+        [Option(name: "repositories", ['r'], Description = "repos.lst file containing list of repositories")]
+            string repositoriesFileName,
+        [Option(name: "template", ['m'], Description = "Template repository to clone")] string templateRepository,
+        [Option(name: "tracking", ['t'], Description = "folder where to write tracking.json file")]
+            string trackingFileName,
+        [Option(name: "work", ['w'], Description = "folder where to clone repositories")] string workFolder
+    )
     {
         IReadOnlyList<string> repositories = await this.LoadRepositoriesAsync(
             repositoriesFileName: repositoriesFileName,
             templateRepository: templateRepository,
-            cancellationToken: this._cancellationToken);
+            cancellationToken: this._cancellationToken
+        );
 
         this.Dump(repositories);
 
-        await this._bulkDependencyReducer.BulkUpdateAsync(templateRepository: templateRepository,
-                                                          trackingFileName: trackingFileName,
-                                                          workFolder: workFolder,
-                                                          repositories: repositories,
-                                                          cancellationToken: this._cancellationToken);
+        await this._bulkDependencyReducer.BulkUpdateAsync(
+            templateRepository: templateRepository,
+            trackingFileName: trackingFileName,
+            workFolder: workFolder,
+            repositories: repositories,
+            cancellationToken: this._cancellationToken
+        );
 
         this.Done();
     }
 
     [Command("check-dependencies", Description = "Reduce dependencies one folder")]
-    
-    public async Task CheckDependenciesAsync([Option(name: "source-folder", ['s'], Description = "folder where the dotnet source is")] string workFolder)
+    public async Task CheckDependenciesAsync(
+        [Option(name: "source-folder", ['s'], Description = "folder where the dotnet source is")] string workFolder
+    )
     {
-        DotNetFiles dotNetFiles = await this._dotNetFilesDetector.FindAsync(baseFolder: workFolder, cancellationToken: this._cancellationToken);
+        DotNetFiles dotNetFiles = await this._dotNetFilesDetector.FindAsync(
+            baseFolder: workFolder,
+            cancellationToken: this._cancellationToken
+        );
 
-        await this._dependencyReducer.CheckReferencesAsync(dotNetFiles: dotNetFiles, new(CommitAsync), cancellationToken: this._cancellationToken);
+        await this._dependencyReducer.CheckReferencesAsync(
+            dotNetFiles: dotNetFiles,
+            new(CommitAsync),
+            cancellationToken: this._cancellationToken
+        );
 
         this.Done();
 
@@ -209,11 +260,16 @@ internal sealed class Commands
         }
     }
 
-    private static IReadOnlyList<string> ExcludeTemplateRepo(IReadOnlyList<string> repositories, string templateRepository)
+    private static IReadOnlyList<string> ExcludeTemplateRepo(
+        IReadOnlyList<string> repositories,
+        string templateRepository
+    )
     {
         return
         [
-            .. repositories.Where(repositoryUrl => !StringComparer.OrdinalIgnoreCase.Equals(x: templateRepository, y: repositoryUrl))
+            .. repositories.Where(repositoryUrl =>
+                !StringComparer.OrdinalIgnoreCase.Equals(x: templateRepository, y: repositoryUrl)
+            ),
         ];
     }
 }
