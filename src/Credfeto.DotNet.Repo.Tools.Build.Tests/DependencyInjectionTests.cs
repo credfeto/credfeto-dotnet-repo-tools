7498e90f66df4e8dedb9958039b24965641036f3
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-03-05 16:37:46 +0000

    Cleanup: DependencyInjectionTests.cs

--- src/Credfeto.DotNet.Repo.Tools.Build.Tests/DependencyInjectionTests.cs
+++ src/Credfeto.DotNet.Repo.Tools.Build.Tests/DependencyInjectionTests.cs
@@ -1,4 +1,4 @@
-﻿using Credfeto.DotNet.Repo.Tools.Build.Interfaces;
+using Credfeto.DotNet.Repo.Tools.Build.Interfaces;
 using FunFair.Test.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -8,9 +8,7 @@ namespace Credfeto.DotNet.Repo.Tools.Build.Tests;
 public sealed class DependencyInjectionTests : DependencyInjectionTestsBase
 {
     public DependencyInjectionTests(ITestOutputHelper output)
-        : base(output: output, dependencyInjectionRegistration: Configure)
-    {
-    }
+        : base(output: output, dependencyInjectionRegistration: Configure) { }
 
     [Fact]
     public void DotNetBuildMustBeRegistered()
@@ -32,7 +30,6 @@ public sealed class DependencyInjectionTests : DependencyInjectionTestsBase
 
     private static IServiceCollection Configure(IServiceCollection services)
     {
-        return services.AddMockedService<IServiceProviderFactory>()
-                       .AddBuild();
+        return services.AddMockedService<IServiceProviderFactory>().AddBuild();
     }
 }
