07184a503f164bd0f896f7ad0754a855ad7eca21
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-06-23 23:53:38 +0000

    Cleanup: DependencyInjectionTests.cs

--- src/Credfeto.DotNet.Repo.Tools.DotNet.Tests/DependencyInjectionTests.cs
+++ src/Credfeto.DotNet.Repo.Tools.DotNet.Tests/DependencyInjectionTests.cs
@@ -1,4 +1,4 @@
-﻿using Credfeto.DotNet.Repo.Tools.DotNet.Interfaces;
+using Credfeto.DotNet.Repo.Tools.DotNet.Interfaces;
 using FunFair.Test.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -8,9 +8,7 @@ namespace Credfeto.DotNet.Repo.Tools.DotNet.Tests;
 public sealed class DependencyInjectionTests : DependencyInjectionTestsBase
 {
     public DependencyInjectionTests(ITestOutputHelper output)
-        : base(output: output, dependencyInjectionRegistration: Configure)
-    {
-    }
+        : base(output: output, dependencyInjectionRegistration: Configure) { }
 
     private static IServiceCollection Configure(IServiceCollection services)
     {
