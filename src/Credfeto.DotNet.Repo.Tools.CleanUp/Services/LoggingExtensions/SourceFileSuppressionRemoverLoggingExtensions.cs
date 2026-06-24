0d549f77be4ca7c2f77f4382dfd7746fff8039c8
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-06-24 07:11:43 +0000

    Cleanup: SourceFileSuppressionRemoverLoggingExtensions.cs

--- src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/LoggingExtensions/SourceFileSuppressionRemoverLoggingExtensions.cs
+++ src/Credfeto.DotNet.Repo.Tools.CleanUp/Services/LoggingExtensions/SourceFileSuppressionRemoverLoggingExtensions.cs
@@ -6,5 +6,10 @@ namespace Credfeto.DotNet.Repo.Tools.CleanUp.Services.LoggingExtensions;
 internal static partial class SourceFileSuppressionRemoverLoggingExtensions
 {
     [LoggerMessage(EventId = 1, Level = LogLevel.Debug, Message = "Cleaning: {filename} Errors occured: {message}")]
-    public static partial void FailedToBuild(this ILogger<SourceFileSuppressionRemover> logger, string filename, string message, Exception exception);
+    public static partial void FailedToBuild(
+        this ILogger<SourceFileSuppressionRemover> logger,
+        string filename,
+        string message,
+        Exception exception
+    );
 }
